74cfb3c5b4d2fb729fc7b288955a6cd3bbecaefd
Bonniesty <43017800+Bonniesty@users.noreply.github.com>
2020-02-06 17:25:13 -0500

    test case

---
+++ test_userTweetSummaryAPI_entry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 17:06:52 2020
+
+@author: tysun
+"""
+
+import pytest
+import userTweetSummaryAPI_entry as test
+
+
+def test_error():
+    assert test.entryAPI("AAA") == "No Account!"
+
+def test_case():
+    assert test.entryAPI("@AnimalPlanet") == "Success!"
+    assert test.entryAPI("@KuoaiBBB") == "Success!"
+    
+if __name__ == "__main__":
+    test_error()
+    test_case()
+    
